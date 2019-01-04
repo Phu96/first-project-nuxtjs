b3ec06aac001569cc721ce14910c075a5a5aad01
unknown <viscafcb96@gmail.com>
2019-01-03 16:59:28 -0800

    optimizes api

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -22,33 +22,41 @@ router.get('/file', (req, res, next) => {
 
 router.post('/file/create', (req, res, next) => {
     fs.readFile(LIST_FILE, (err, data) => {
-        let list = JSON.parse(data);
-        list.push(req.body)
-         fs.appendFile(pathFileFolder + `/${req.body.fileName}.txt`, req.body.contentFile, err=> {
-            if (err) console.log(err);
-         })
-         fs.writeFile(LIST_FILE, JSON.stringify(list, null, 4), err => {
-             if(err) console.log(err)
-             res.json(list);
-         })
+        if (err) {
+            console.log(err);
+            return
+        }else {
+            let list = JSON.parse(data);
+            list.push(req.body)
+            fs.appendFile(pathFileFolder + `/${req.body.fileName}.txt`, req.body.contentFile, err=> {
+                if (err) console.log(err);
+                fs.writeFile(LIST_FILE, JSON.stringify(list, null, 4), err => {
+                    if(err) console.log(err)
+                    res.json(list);
+                })
+            })
+        }
     })
 })
 
 
 router.post('/file/delete', (req, res, next) => {
     fs.readFile(LIST_FILE, (err, data) => {
-        let list = JSON.parse(data);
-        console.log(req.body.index);
-        fs.remove(pathFileFolder + `/${list[req.body.index].fileName}.txt`, err => {
-            if(err) console.log(err)
-            console.log('file deleted')
-        })
-        list.splice(req.body.index, 1);
-         
-         fs.writeFile(LIST_FILE, JSON.stringify(list, null, 4), err => {
-             if(err) console.log(err)
-             res.json(list);
-         })
+        if(err) {
+            console.log(err);
+            return
+        }else {
+            let list = JSON.parse(data);
+            fs.remove(pathFileFolder + `/${list[req.body.index].fileName}.txt`, err => {
+                if(err) console.log(err)
+                console.log('file deleted')
+                list.splice(req.body.index, 1);
+                fs.writeFile(LIST_FILE, JSON.stringify(list, null, 4), err => {
+                    if(err) console.log(err)
+                    res.json(list);
+                })
+            })
+        }
     })
 })
 
